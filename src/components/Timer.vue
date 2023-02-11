6a6cffe99645b7b8a69e4bf9dc4162718308d807
BayuDC <dev.bayudc@gmail.com>
2023-02-11 22:55:55 +0700

    feat: a working countdown

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,16 +1,59 @@
 <script lang="ts" setup>
+import { onMounted, onUpdated, ref } from 'vue';
 import Item from './TimerItem.vue';
 import Divider from './TimerDivider.vue';
+
+const emit = defineEmits<{
+    (e: 'finish'): void;
+}>();
+
+const second = ref<number>(0);
+const minute = ref<number>(0);
+const hour = ref<number>(0);
+const day = ref<number>(0);
+
+const countdown = ref<any | null>();
+
+onMounted(() => {
+    if (day.value == 0 && hour.value == 0 && minute.value == 0 && second.value == 0) {
+        return emit('finish');
+    }
+    countdown.value = setInterval(() => {
+        second.value--;
+    }, 1000);
+});
+
+onUpdated(() => {
+    if (day.value == 0 && hour.value == 0 && minute.value == 0 && second.value == 0) {
+        clearInterval(countdown.value);
+        return emit('finish');
+    }
+});
+
+function handleHourFinish() {
+    day.value--;
+    hour.value = 23;
+}
+function handleMinuteFinish() {
+    hour.value--;
+    minute.value = 59;
+}
+function handleSecondFinish() {
+    minute.value--;
+    second.value = 59;
+}
 </script>
 
 <template>
     <div class="flex flex-col items-center md:items-start justify-center gap-4 lg:gap-6 md:flex-row">
-        <Item :number="31" label="Hari" />
+        <template v-if="day">
+            <Item :number="day" label="Hari" />
+            <Divider />
+        </template>
+        <Item :number="hour" label="Jam" @finish="handleHourFinish" />
         <Divider />
-        <Item :number="23" label="Jam" />
+        <Item :number="minute" label="Menit" @finish="handleMinuteFinish" />
         <Divider />
-        <Item :number="59" label="Menit" />
-        <Divider />
-        <Item :number="59" label="Detik" />
+        <Item :number="second" label="Detik" @finish="handleSecondFinish" />
     </div>
 </template>
--- src/components/TimerItem.vue
+++ src/components/TimerItem.vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
-defineProps<{
+import { computed } from 'vue';
+
+const props = defineProps<{
     number: number;
     label: string;
 }>();
+
+const emit = defineEmits<{
+    (e: 'finish'): void;
+}>();
+
+const number = computed(() => {
+    if (props.number < 0) {
+        return emit('finish');
+    }
+    if (props.number < 10) {
+        return '0' + props.number.toString();
+    }
+    return props.number.toString();
+});
 </script>
 
 <template>
     <div class="flex flex-col items-center">
-        <div class="text-[80px] md:text-[96px] lg:text-[128px] leading-none flex text-center">
-            <div class="w-14 md:w-16 lg:w-20">
-                {{ number.toString().charAt(0) }}
-            </div>
-            <div class="w-14 md:w-16 lg:w-20">
-                {{ number.toString().charAt(1) }}
+        <div class="text-[80px] md:text-[96px] lg:text-[128px] leading-none flex text-right">
+            <div v-for="char in number" class="w-14 md:w-16 lg:w-20">
+                {{ char }}
             </div>
         </div>
         <div class="md:self-end md:text-lg lg:text-xl md:mr-2">{{ label }}</div>
